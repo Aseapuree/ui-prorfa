24b96a81e8dc459ebae785798712db5b4b51087d
fernandoisaias003 <diazmfer159@gmail.com>
2025-02-11 01:43:55 -0500

    crear ruta para principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './general/principal/principal.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [{ path: '', component: PrincipalComponent}];
--- src/app/general/principal/principal.component.ts
+++ src/app/general/principal/principal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent,CommonModule],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
