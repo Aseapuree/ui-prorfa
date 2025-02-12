ef2ca6b57688e3ecf42730ba9fd4000818d7d640
luis monsalve <joisermonsalve23@gmail.com>
2025-02-12 07:46:41 -0500

    Creacion de las card sesiones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CampusComponent } from './components/modules/campus/campus.component';
+import { Sesion1Component } from './components/modules/courses/sesion1/sesion1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: CampusComponent, pathMatch: 'full'},
+    { path: 'curso-angular', component: Sesion1Component}
+];
---
+++ src/app/components/modules/courses/sesion1/sesion1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sesion1',
+  standalone: true,
+  imports: [],
+  templateUrl: './sesion1.component.html',
+  styleUrl: './sesion1.component.scss'
+})
+export class Sesion1Component {
+
+}
