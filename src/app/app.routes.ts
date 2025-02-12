7e6bec4542d3f993ccaea870d156f8c284a1d8cc
luis monsalve <joisermonsalve23@gmail.com>
2025-02-12 08:01:42 -0500

    Actualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 import { PrincipalComponent } from './general/principal/principal.component';
 
 export const routes: Routes = [
+    {path: '', component: PrincipalComponent }
 ];
