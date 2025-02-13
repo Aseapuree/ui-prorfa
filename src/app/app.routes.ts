ef699860c5dd34ebc9b75d16d42f24e8b555b99c
fernandoisaias003 <diazmfer159@gmail.com>
2025-02-13 00:59:01 -0500

    se modifica y elimino el main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,5 @@ import { Routes } from '@angular/router';
 import { PrincipalComponent } from './general/principal/principal.component';
 
 
-export const routes: Routes = [{ path: '', component: PrincipalComponent}];
+
+export const routes: Routes = [{ path: '', component: PrincipalComponent, }];
--- src/app/general/principal/principal.component.ts
+++ src/app/general/principal/principal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-principal',
   standalone: true,
