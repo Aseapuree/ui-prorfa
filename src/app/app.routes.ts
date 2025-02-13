04a6dae557eed381dd6311ee2371ac833be6b932
user-jam <jampierrets@gmail.com>
2025-02-13 01:43:23 -0500

    update commit jam

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { PrincipalComponent } from './general/principal/principal.component';
 import { CampusComponent } from './components/modules/campus/campus.component';
 import { Sesion1Component } from './components/modules/courses/sesion1/sesion1.component';
+import { LayoutComponent } from './general/layout/layout.component';
 
 export const routes: Routes = [
-    {path: '', component: PrincipalComponent },
-    {path: 'campus', component: CampusComponent},
+    {path: '', component: LayoutComponent, children:[
+        {path:'campus',component:CampusComponent}
+    ] },
+    // {path: 'campus', component: CampusComponent},
     { path: 'curso-angular', component: Sesion1Component}
 ];
--- src/app/general/principal/principal.component.ts
+++ src/app/general/principal/principal.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-principal',
   standalone: true,
-  imports: [MenuComponent,CommonModule],
+  imports: [MenuComponent,CommonModule,RouterOutlet],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.scss'
 })
