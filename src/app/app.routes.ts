8c63ecb62691ce2e42618f5f73f90711b7c58676
user-jam <jampierrets@gmail.com>
2025-02-13 14:25:06 -0500

    update container

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { PrincipalComponent } from './general/principal/principal.component';
 import { CampusComponent } from './components/modules/campus/campus.component';
 import { Sesion1Component } from './components/modules/courses/sesion1/sesion1.component';
 import { LayoutComponent } from './general/layout/layout.component';
@@ -8,6 +7,5 @@ export const routes: Routes = [
     {path: '', component: LayoutComponent, children:[
         {path:'campus',component:CampusComponent}
     ] },
-    // {path: 'campus', component: CampusComponent},
     { path: 'curso-angular', component: Sesion1Component}
 ];
