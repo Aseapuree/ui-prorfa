9c2e7a3f208f465418f2acb69d9acfcc57f2aae2
fernandoisaias003 <diazmfer159@gmail.com>
2026-01-27 23:48:13 -0500

    commit prueba

--- src/app/matricula/services/alumno.service.ts
+++ src/app/matricula/services/alumno.service.ts
@@ -2,13 +2,14 @@ import { Alumno } from '../interfaces/DTOAlumno';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, map, Observable, throwError } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlumnoService {
 
-  private urlBase = "/api/v1/alumnos";
+  private urlBase = `${environment.apiUrl}/api/v1/alumnos`;
 
   constructor(private http: HttpClient) { }
 
